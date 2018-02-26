6ea43bb5aecab34e5d72a1a75cee1b1dcf88214e
thiry <thiry@five-seven.net>
2018-02-26 14:57:21 +0900

    feat(Player): add Player info
    add Player info
    
    チケットはありません

--- src/views/player/Player.tsx
+++ src/views/player/Player.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { KdaProps } from "../kda/Kda";
+import {Kda, KdaProps} from "../kda/Kda";
+import { GameStateIntegration } from "../../dataTypes";
+import Team = GameStateIntegration.Team;
+const classNames = require("./player.scss");
 export interface PlayerProps {
     /**
      * プレイヤーの名前.
@@ -61,24 +64,119 @@ export interface PlayerProps {
          * HE の保有数.
          */
         highExplosiveAmount: number;
-    }
+        /**
+         * molotov の保有数.
+         */
+        molotovAmount: number;
+        /**
+         * decoy の保有数.
+         */
+        decoyAmount: number;
+    };
+    // /**
+    //  * 装備に使用した合計金額.
+    //  */
+    // equipmentValue: number;
+    // /**
+    //  * 装備に使用した合計金額を表示するか.
+    //  */
+    // showEquipmentValue: boolean;
     /**
-     * 装備に使用した合計金額.
+     * チーム.
      */
-    equipmentValue: number;
-    /**
-     * 装備に使用した合計金額を表示するか.
-     */
-    showEquipmentValue: boolean;
+    team: Team;
 }
+const createHighExplosiveAmountInfo = (props: PlayerProps): JSX.Element => {
+    if (props.weapon.highExplosiveAmount === 0) {
+        return null;
+    }
+    return (
+        <span className={classNames.highExplosive}>{props.weapon.highExplosiveAmount}</span>
+    );
+};
+const createSmokeAmountInfo = (props: PlayerProps): JSX.Element => {
+    if (props.weapon.smokeAmount === 0) {
+        return null;
+    }
+    return (
+        <span className={classNames.smoke}>{props.weapon.smokeAmount}</span>
+    );
+};
+const createMolotovAmountInfo = (props: PlayerProps): JSX.Element => {
+    if (props.weapon.molotovAmount === 0) {
+        return null;
+    }
+    return (
+        <span className={classNames.molotov}>{props.weapon.molotovAmount}</span>
+    );
+};
+const createDecoyAmountInfo = (props: PlayerProps): JSX.Element => {
+    if (props.weapon.molotovAmount === 0) {
+        return null;
+    }
+    return (
+        <span className={classNames.decoy}>{props.weapon.decoyAmount}</span>
+    );
+};
+const createFlashBangAmountInfo = (props: PlayerProps): JSX.Element[] => {
+    if (props.weapon.flashBangAmount === 0) {
+        return null;
+    }
+    return [...Array(props.weapon.flashBangAmount).keys()].map(key => (
+        <span
+            className={classNames.flashBang}
+            key={key}
+        >
+            {props.weapon.flashBangAmount}
+        </span>
+    ));
+};
+const createArmorInfo = (props: PlayerProps): JSX.Element => {
+    if (props.hasHelmet && props.armor > 0) {
+        return <span className={classNames.armorWithHelmet} />;
+    } else if (props.armor > 0) {
+        return <span className={classNames.armor} />;
+    }
+};
 /**
  * プレイヤーコンポーネント
  * @param  {PlayerProps} props
  */
 export const Player: React.StatelessComponent<PlayerProps> = (props: PlayerProps) => {
     return (
-        <div data-is-alive={props.health !== 0}>
-
+        <div
+            className={classNames.player}
+            data-is-alive={props.health !== 0}
+            data-team={props.team}
+        >
+            <div className={classNames.mainInfo}>
+                <span className={classNames.healthBar} data-team={props.team} />
+                <span className={classNames.health}>{props.health}</span>
+                <span className={classNames.name}>{props.name}</span>
+                <span
+                    className={classNames.primaryWeapon}
+                    data-is-active={props.weapon.activeWeapon === props.weapon.primary}
+                >
+                    {props.weapon.primary}
+                </span>
+            </div>
+            <div className={classNames.subInfo}>
+                {createArmorInfo(props)}
+                <span className={classNames.money}>{props.money}</span>
+                <span className={classNames.roundKillCount}>{props.roundKillCount}</span>
+                {createHighExplosiveAmountInfo(props)}
+                {createSmokeAmountInfo(props)}
+                {createFlashBangAmountInfo(props)}
+                {createMolotovAmountInfo(props)}
+                {createDecoyAmountInfo(props)}
+                <span
+                    className={classNames.secondaryWeapon}
+                    data-is-active={props.weapon.activeWeapon === props.weapon.secondary}
+                >
+                    {props.weapon.secondary}
+                </span>
+            </div>
+            {props.showKda && <Kda {...props.kda} />}
         </div>
     );
 };
--- src/views/player/Story.tsx
+++ src/views/player/Story.tsx
@@ -1,8 +1,32 @@
 import * as React from "react";
-import { Player } from "./Player";
+import { Player, PlayerProps } from "./Player";
 import { storiesOf, Story } from "@storybook/react";
+import { props as kdaProps } from "../kda/Story";
+import { GameStateIntegration } from "../../dataTypes";
+import Team = GameStateIntegration.Team;
+const props: PlayerProps = {
+    name: "Foo",
+    money: 16000,
+    health: 100,
+    armor: 100,
+    hasHelmet: true,
+    showKda: true,
+    kda: kdaProps,
+    roundKillCount: 1,
+    weapon: {
+        activeWeapon: "weapon_ak47",
+        primary: "weapon_ak47",
+        secondary: "weapon_p2000",
+        flashBangAmount: 1,
+        smokeAmount: 1,
+        highExplosiveAmount: 1,
+        decoyAmount: 0,
+        molotovAmount: 1,
+    },
+    team: Team.T,
+};
 
-// storiesOf("Player", module)
-//     .add("プレイヤー情報を表示できる", () => {
-//         return <Player />;
-//     });
+storiesOf("Player", module)
+    .add("プレイヤー情報を表示できる", () => {
+        return <Player {...props} />;
+    });
