c87afd8d1bf7161eb4726777120436f43d1fdff3
thiry <thiry@five-seven.net>
2018-03-04 23:03:20 +0900

    perf(KDA): BaseComponentを継承させた
    BaseComponentを継承させた
    
    チケットはありません

--- src/views/kda/Kda.tsx
+++ src/views/kda/Kda.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { GameStateIntegration } from "../../dataTypes";
 import Team = GameStateIntegration.Team;
+import { BaseComponent } from "../util/baseComponent";
 const classNames = require("./kda.scss");
 export interface KdaProps {
     className?: string;
@@ -25,27 +26,29 @@ export interface KdaProps {
  * KDAコンポーネント
  * @param  {KdaProps} props
  */
-export const Kda: React.StatelessComponent<KdaProps> = (props: KdaProps) => {
-    return (
-        <div
-            className={props.className || classNames.kda}
-            data-team={props.team}
-        >
-            <div className={classNames.column}>
-                <div className={classNames.title}>K</div>
-                <div className={classNames.value}>{props.kill}</div>
+export class Kda extends BaseComponent<KdaProps, {}> {
+    render() {
+        return (
+            <div
+                className={this.props.className || classNames.kda}
+                data-team={this.props.team}
+            >
+                <div className={classNames.column}>
+                    <div className={classNames.title}>K</div>
+                    <div className={classNames.value}>{this.props.kill}</div>
+                </div>
+                <div className={classNames.column}>
+                    <div className={classNames.title}>A</div>
+                    <div className={classNames.value}>{this.props.assist}</div>
+                </div>
+                <div className={classNames.column}>
+                    <div className={classNames.title}>D</div>
+                    <div className={classNames.value}>{this.props.death}</div>
+                </div>
             </div>
-            <div className={classNames.column}>
-                <div className={classNames.title}>A</div>
-                <div className={classNames.value}>{props.assist}</div>
-            </div>
-            <div className={classNames.column}>
-                <div className={classNames.title}>D</div>
-                <div className={classNames.value}>{props.death}</div>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export {
     Kda as Component,
