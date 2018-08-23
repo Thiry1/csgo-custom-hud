8833f8e10f41e77e40a8ad2e12832da69aaf7521
execut1ve <execut1ven0rm4u@gmail.com>
2018-08-23 14:57:24 +0900

    アーマーのみの指定を削除

--- src/util/armorIconResolver.ts
+++ src/util/armorIconResolver.ts
@@ -2,8 +2,6 @@ export namespace ArmorIconResolver {
     export const resolve = (info: { hasHelmet: boolean, armor: number }): string | null => {
         if (info.hasHelmet && info.armor > 0) {
             return require("../resources/armors/helmet.png");
-        } else if (info.armor > 0) {
-            return require("../resources/armors/armor.png");
         } else {
             return require("../resources/armors/armor.png");
         }
